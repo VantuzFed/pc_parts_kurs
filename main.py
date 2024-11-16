15b2fbb72e9a42e9bc0891c958a877847d75dc24
vantuzfed <ivan238777@gmail.com>
2024-11-16 13:01:54 +0300

    fixed basic form

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from flask_wtf import FlaskForm
 from markupsafe import Markup
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from flask_wtf.csrf import CSRFProtect
+
 
 
 class LoginForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
-
 
 mydb = connect(
     host='127.0.0.1',
@@ -17,16 +18,17 @@ mydb = connect(
 )
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '57f347f0c3f3d5970a7d5548'
+csrf = CSRFProtect(app)
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm(meta={'csrf': False})
+    form = LoginForm()
     if request.method == 'POST' and form.validate():
-        flash(f'{form.name}')
+        return form.name.data
     return render_template('login.html', form=form)
 
 if __name__ == '__main__':
