dc24829a314b44dd60ee1013adf51247d85a9182
vantuzfed <ivan238777@gmail.com>
2024-11-23 23:56:02 +0300

    обновление бд и схемы

--- db_mysql.sql
+++ db_mysql.sql
@@ -11,7 +11,7 @@ CREATE TABLE `Users` (
   `e_mail` varchar(30) UNIQUE NOT NULL,
   `phone_number` varchar(15) NOT NULL,
   `password_` varchar(20) NOT NULL,
-  `account_type` enum('User', 'Admin') DEFAULT 'User',
+  `account_type` enum('User','Admin') DEFAULT 'User',
   `profile_image` varchar(20)
 );
 
@@ -36,8 +36,8 @@ CREATE TABLE `Components` (
 
 CREATE TABLE `Warehouse_stock` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `warehouse_id` int unique,
-  `component_id` int unique,
+  `warehouse_id` int UNIQUE,
+  `component_id` int UNIQUE,
   `quantity` int NOT NULL,
   `last_updated` timestamp DEFAULT CURRENT_TIMESTAMP
 );
@@ -52,15 +52,15 @@ CREATE TABLE `Suppliers` (
 
 CREATE TABLE `Supplier_components` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `supplier_id` int unique,
-  `component_id` int unique,
+  `supplier_id` int UNIQUE,
+  `component_id` int UNIQUE,
   `supply_price` numeric(10,2) NOT NULL,
   `supply_date` timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `Orders` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `user_id` int unique,
+  `user_id` int UNIQUE,
   `order_date` timestamp DEFAULT CURRENT_TIMESTAMP,
   `status` varchar(50) DEFAULT 'В обработке',
   `total_price` numeric(10,2) DEFAULT 0
@@ -68,22 +68,23 @@ CREATE TABLE `Orders` (
 
 CREATE TABLE `Order_details` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `order_id` int unique,
-  `component_id` int unique,
+  `order_id` int UNIQUE,
+  `component_id` int UNIQUE,
   `quantity` int,
   `unit_price` numeric(10,2)
 );
 
-ALTER TABLE `Warehouses` ADD FOREIGN KEY (`id`) REFERENCES `Warehouse_stock` (`warehouse_id`) ON DELETE CASCADE;
+ALTER TABLE `Warehouse_stock` ADD FOREIGN KEY (`warehouse_id`) REFERENCES `Warehouses` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `Components` ADD FOREIGN KEY (`id`) REFERENCES `Warehouse_stock` (`component_id`) ON DELETE CASCADE;
+ALTER TABLE `Warehouse_stock` ADD FOREIGN KEY (`component_id`) REFERENCES `Components` (`id`) ON DELETE CASCADE;
 
 ALTER TABLE `Supplier_components` ADD FOREIGN KEY (`component_id`) REFERENCES `Components` (`id`);
 
-ALTER TABLE `Suppliers` ADD FOREIGN KEY (`id`) REFERENCES `Supplier_components` (`supplier_id`) ON DELETE CASCADE;
+ALTER TABLE `Supplier_components` ADD FOREIGN KEY (`supplier_id`) REFERENCES `Suppliers` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `Users` ADD FOREIGN KEY (`id`) REFERENCES `Orders` (`user_id`) ON DELETE CASCADE;
+ALTER TABLE `Orders` ADD FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE;
 
 ALTER TABLE `Order_details` ADD FOREIGN KEY (`component_id`) REFERENCES `Components` (`id`);
 
 ALTER TABLE `Order_details` ADD FOREIGN KEY (`order_id`) REFERENCES `Orders` (`id`) ON DELETE CASCADE;
+
