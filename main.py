e1d486f2a18e991790e070a4a3287d189a57fc4b
vantuzfed <ivan238777@gmail.com>
2024-11-24 19:52:22 +0300

    запросы переписаны под орм

--- main.py
+++ main.py
@@ -3,23 +3,18 @@ from flask import Flask, render_template, flash, request
 from db_forms import *
 from markupsafe import Markup
 from flask_wtf.csrf import CSRFProtect
-from mysql.connector import connect
 import os
-
-try:
-        mydb = connect(
-        host='127.0.0.1',
-        user='root',
-        password='35678',
-        db='pc_parts'
-    )
-except:
-    print('db connection error, aborted')
-    exit(1)
+from sqlalchemy import  create_engine, or_
+from sqlalchemy.orm import sessionmaker
+from models import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(12).hex()
 csrf = CSRFProtect(app)
+
+engine = create_engine("mysql+pymysql://root:35678@127.0.0.1/pc_parts")
+Session = sessionmaker(bind=engine)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -30,16 +25,15 @@ def login():
     if request.method == 'POST' and form.validate():
         login_str = form.login.data
         password_str = form.password.data
-        cur = mydb.cursor()
-        cur.execute('select * from users where login = \'{}\' and password_ = \'{}\''.format(login_str, password_str))
-        res = cur.fetchone()
-        if type(res) is NoneType:
+        with Session() as db:
+            user = db.query(Users).filter(or_(Users.login == login_str, Users.password_ == password_str)).first()
+        if user:
+            if user.login == login_str or user.password_ == password_str:
+                flash(Markup('<h3>Вы успешно зашли в аккаунт</h3>'))
+                flash(Markup(f'<p>Тип аккаунта: {user.account_type}</p>'))
+        else:
             flash(Markup('<h3>Неверное имя пользователя или пароль</h3>'))
-        elif res[1] == login_str or res[6] == password_str:
-            flash(Markup('<h3>Вы успешно зашли в аккаунт</h3>'))
-            for i in range(1,len(res),1):
-                flash(Markup(f'<p>{res[i]}</p>'))
-        cur.close()
+
     return render_template('login.html', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -52,18 +46,18 @@ def register():
         l_name_str = form.last_name.data
         phone_str = form.phone.data
         password_str = form.password.data
-        cur = mydb.cursor()
-        cur.execute('select * from users where login = \'{}\' or e_mail = \'{}\''.format(login_str, e_mail_str))
-        res = cur.fetchone()
-        if type(res) is NoneType:
-            val = (login_str, f_name_str, l_name_str, e_mail_str, phone_str, password_str)
-            sql = 'INSERT INTO users (login, first_name, last_name, e_mail, phone_number, password_) VALUES(%s,%s,%s,%s,%s,%s)'
-            cur.execute(sql, val)
-            mydb.commit()
+        with Session() as db:
+            user = db.query(Users).filter(or_(Users.login == login_str, Users.e_mail == e_mail_str)).first()
+        if user:
+            if user.login == login_str or user.e_mail == e_mail_str:
+                flash(Markup('<h3>Такой пользователь уже существует</h3>'))
+        else:
+            with Session() as db:
+                new_user = Users(login=login_str, first_name=f_name_str, last_name=l_name_str, e_mail=e_mail_str, phone_number=phone_str, password_=password_str)
+                db.add(new_user)
+                db.commit()
             flash(Markup('<h3>Пользователь успешно создан</h3>'))
-        elif res[1] == login_str or res[4] == e_mail_str:
-            flash(Markup('<h3>Такой пользователь уже существует</h3>'))
-        cur.close()
+
     return render_template('register.html', form=form)
 
 @app.route('/catalog')
