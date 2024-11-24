84b6650fcdcc524e3d4621350923935f2b368092
vantuzfed <ivan238777@gmail.com>
2024-11-24 22:26:38 +0300

    новые формы

--- db_forms.py
+++ db_forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, IntegerField, FloatField
 from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
@@ -14,3 +14,20 @@ class RegisterForm(FlaskForm):
     last_name = StringField('Фамилия:', validators=[DataRequired(), Length(min=3, max=20)])
     phone = StringField('Номер телефона:', validators=[DataRequired(), Length(min=11, max=15)])
     password = PasswordField('Пароль:', validators=[DataRequired(), Length(min=4, max=20)])
+
+class SuppliersForm(FlaskForm):
+    name = StringField('Название поставщика:', validators=[DataRequired(), Length(min=5, max=60)])
+    e_mail = StringField('E-mail:', validators=[DataRequired(), Email(message="Введите корректный адресс электронной почты")])
+    phone = StringField('Номер телефона:', validators=[DataRequired(), Length(min=11, max=15)])
+    address = StringField('Адрес поставщика:', validators=[DataRequired(), Length(min=4, max=100)])
+
+class WarehousesForm(FlaskForm):
+    name = StringField('Название склада:', validators=[DataRequired(), Length(min=5, max=60)])
+    address = StringField('Адрес склада:', validators=[DataRequired(), Length(min=4, max=100)])
+    capacity = IntegerField('Вместимость склада:', validators=[DataRequired()])
+
+class ComponentsForm(FlaskForm):
+    vendors = StringField('Производитель запчасти:', validators=[DataRequired(), Length(min=5, max=60)])
+    model = StringField('Модель запчасти:', validators=[DataRequired(), Length(min=5, max=60)])
+    type = StringField('Тип:', validators=[DataRequired(), Length(min=5, max=60)])
+    price = FloatField('Цена запчасти:', validators=[DataRequired()])
